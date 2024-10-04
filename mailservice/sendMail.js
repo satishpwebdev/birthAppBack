4de0c71cf628a7bd1402ede393744cd247030b24
satish <satishwillowood@gmail.com>
2024-10-04 16:38:14 +0530

    second commit

--- mailservice/sendMail.js
+++ mailservice/sendMail.js
@@ -3,20 +3,22 @@ const nodemailer = require("nodemailer");
 
 // Configure the SMTP settings
 const transporter = nodemailer.createTransport({
-  service: 'gmail', 
+  service: "gmail",
   auth: {
-      user: 'satishwillowood@gmail.com',
-      pass: 'nish vouu klpz qegy'    
+    // user: 'satishwillowood@gmail.com',
+    // pass: 'nish vouu klpz qegy',
+
+    user: process.env.APP_EMAIL,
+    pass: process.env.APP_PASS
   }
 
-// host: "smtp-relay.brevo.com",
-//     port: 587,
-//     secure: false, 
-//     auth: {
-//       user: "7d1d03002@smtp-brevo.com", 
-//       pass: "dE9pDJPK6TfmqGWN", 
-//     },
-  
+  // host: "smtp-relay.brevo.com",
+  //     port: 587,
+  //     secure: false,
+  //     auth: {
+  //       user: "7d1d03002@smtp-brevo.com",
+  //       pass: "dE9pDJPK6TfmqGWN",
+  //     },
 });
 
 const sendMail = (to, name) => {
