33bfe62d931a583c41651b95fd91d600f1a252e0
satish <satishwillowood@gmail.com>
2024-10-18 18:35:00 +0530

    added middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,12 +1,23 @@
 const ObjectId = require("mongoose").Types.ObjectId;
 
 const validateObjID = (req, res, next) => {
-   if (ObjectId.isValid(req.params.id) === false) {
-      const error = new Error(`Given id is not valid: ${req.params.id}`);
-      error.status = 400;
-      return next(error);
-   }
-   next();
+  if (ObjectId.isValid(req.params.id) === false) {
+    const error = new Error(`Given id is not valid: ${req.params.id}`);
+    error.status = 400;
+    return next(error);
+  }
+  next();
+};
+
+const apiKeyMiddleware = (req, res, next) => {
+  const apiKey = req.headers["x-api-key"];
+  const validApiKey = process.env.API_KEY;
+
+  if (!apiKey || apiKey !== validApiKey) {
+    return res.status(403).json({ message: "Forbidden. Invalid or missing API key." });
+  }
+
+  next();
 };
 
 // const respHandle =(req,res)=>{
@@ -15,19 +26,20 @@ const validateObjID = (req, res, next) => {
 //    })
 // }
 
-const res404Handle =(req,res)=>{
-   res.status(404).json({
-      error: `No records for this ${req.params.id}`
-   })
-}
+const res404Handle = (req, res) => {
+  res.status(404).json({
+    error: `No records for this ${req.params.id}`
+  });
+};
 
-const errorHandler = (error, req, res, next)=>{
-   res.status(500).json({error})
-}
+const errorHandler = (error, req, res, next) => {
+  res.status(500).json({ error });
+};
 
 module.exports = {
-    validateObjID,
-    res404Handle,
-    errorHandler,
-   //  respHandle
-}
+  validateObjID,
+  res404Handle,
+  errorHandler,
+  apiKeyMiddleware
+  //  respHandle
+};
--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const {getAllTasks,getTaskById,createTask, updateTask, deleteTask} = require('../controllers/taskController')
 const {bulkUploadTasks} = require('../controllers/bulkController')
-const {validateObjID} =require('../middlewares')
+const {validateObjID, apiKeyMiddleware} =require('../middlewares')
 
 
-router.get('/tasks', getAllTasks)
+router.get('/tasks', apiKeyMiddleware, getAllTasks)
 router.get('/tasksbyid/:id?',validateObjID, getTaskById)
-router.post('/tasks', createTask)
+router.post('/tasks', apiKeyMiddleware, createTask)
 router.put('/tasksup/:id?',validateObjID, updateTask)
 router.delete('/taskdel/:id?', validateObjID, deleteTask)
 
